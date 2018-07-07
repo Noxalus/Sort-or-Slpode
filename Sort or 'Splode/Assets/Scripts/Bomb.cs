b51c262f4bcae34daf53e886750e8822bdd055f4
Noxalus <noxalus@gmail.com>
2018-07-07 14:37:50 +0200

    Handle basic bomb's collision.

---
+++ Sort or 'Splode/Assets/Scripts/Bomb.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public Rigidbody2D RigidBody;
+
+    private float _speed;
+
+	void Start ()
+    {
+        _speed = 5;
+        RigidBody.velocity = new Vector2(_speed, _speed);
+    }
+
+	void Update ()
+    {
+	}
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        var normal = collision.contacts[0].normal;
+
+        RigidBody.velocity = Vector3.Reflect(RigidBody.velocity, normal);
+    }
+}
