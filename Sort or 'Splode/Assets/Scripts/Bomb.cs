916f2ab2f973e9615758e1404d48b35617098166
Noxalus <noxalus@gmail.com>
2018-07-07 14:51:39 +0200

    Add drag and drop behaviour to the bomb.

--- Sort or 'Splode/Assets/Scripts/Bomb.cs
+++ Sort or 'Splode/Assets/Scripts/Bomb.cs
@@ -5,8 +5,10 @@ public class Bomb : MonoBehaviour
     public Rigidbody2D RigidBody;
 
     private float _speed;
+    private bool _dragging;
+    private Vector3 _offset;
 
-	void Start ()
+    void Start ()
     {
         _speed = 1;
         RigidBody.velocity = new Vector2(_speed, _speed);
@@ -16,10 +18,31 @@ public class Bomb : MonoBehaviour
     {
 	}
 
+    private void OnMouseDown()
+    {
+        _dragging = true;
+        _offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+    }
+
+    private void OnMouseUp()
+    {
+        _dragging = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+        RigidBody.MovePosition(Camera.main.ScreenToWorldPoint(newPosition) + _offset);
+    }
+
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        var normal = collision.contacts[0].normal;
-
-        RigidBody.velocity = Vector3.Reflect(RigidBody.velocity, normal);
+        // When the bomb hits a wall
+        if (!_dragging)
+        {
+            var normal = collision.contacts[0].normal;
+            RigidBody.velocity = Vector3.Reflect(RigidBody.velocity, normal);
+        }
     }
 }
